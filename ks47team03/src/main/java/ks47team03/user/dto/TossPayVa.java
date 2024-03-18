eab03d79bacbb583f4b66476a639da2364267efe
SUBONG <chltnqhd010@naver.com>
2024-03-18 21:29:38 +0900

    환급 쿼리 수정필요

---
+++ ks47team03/src/main/java/ks47team03/user/dto/TossPayVa.java
@@ -0,0 +1,17 @@
+/*
+package ks47team03.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class TossPayVa {
+    private Response response;
+
+    @Data
+    @AllArgsConstructor
+    public class Response {
+    private
+    }
+}*/
